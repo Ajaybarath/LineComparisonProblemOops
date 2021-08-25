820ff02e78c39cb8cf102963fee41bfbb889f925
ajaybarath <ajaybarath3@gmail.com>
2021-08-25 19:51:08 +0530

    ADDED: line comparison done using equals method

--- src/com/bridgeLabz/lineComparison/LineComparison.java
+++ src/com/bridgeLabz/lineComparison/LineComparison.java
@@ -9,10 +9,25 @@ public class LineComparison {
 		int y1 = 3;
 		int y2 = 5;
 		
-		int lengthOfLine = (int) Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+		int x3 = 30;
+		int x4 = 3;
+		int y3 = 7;
+		int y4 = 5;
 		
-		System.out.println("The line length is " + lengthOfLine);
-			
+		Integer lengthOfLine1 = (int) Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+		
+		Integer lengthOfLine2 = (int) Math.sqrt(Math.pow((x4-x3), 2) + Math.pow((y4-y3), 2));
+		
+		System.out.println(lengthOfLine2);
+		System.out.println(lengthOfLine1);
+
+		if (lengthOfLine1.equals(lengthOfLine2)) {
+			System.out.println("The lines are equal");
+		}
+		else {
+			System.out.println("Lines are not equal");
+		}
+		
 	}
 
 }
